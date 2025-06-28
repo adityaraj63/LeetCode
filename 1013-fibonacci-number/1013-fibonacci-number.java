76c1643fb1f8f2762358c294630ba1ec76107fef
adityaraj63 <164489231+adityaraj63@users.noreply.github.com>
2025-06-28 11:06:44 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.24%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    // USING DYNAMIC PROGRAMMING:
+    public int fib(int n, int [] dp) {
+        if (n <= 1) return n;
+        if (dp[n] != 0) return dp[n]; //NEW
+        int ans = fib(n-1, dp) + fib(n-2, dp);
+        dp[n] = ans;
+        return ans;
+    }
+    public int fib(int n) {
+        int [] dp = new int[n+1]; //INDEX FROM 0-N:
+        return fib(n, dp);
+    }
+}
