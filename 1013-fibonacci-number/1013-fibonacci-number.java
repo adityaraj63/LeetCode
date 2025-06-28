8cc291140b5a4445abe47d77280306ece9e35754
adityaraj63 <164489231+adityaraj63@users.noreply.github.com>
2025-06-28 11:22:04 +0530

    Time: 0 ms (100%), Space: 40.8 MB (11.36%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,12 @@
 class Solution {
-    // USING DYNAMIC PROGRAMMING:
-    public int fib(int n, int [] dp) {
-        if (n <= 1) return n;
-        if (dp[n] != 0) return dp[n]; //NEW
-        int ans = fib(n-1, dp) + fib(n-2, dp);
-        dp[n] = ans;
-        return ans;
-    }
     public int fib(int n) {
-        int [] dp = new int[n+1]; // MEMOIZATION ARRAY-> INDEX FROM 0-N:
-        return fib(n, dp);
+        // USING DYNAMIC PROGRAMMING:-> (TABULATION)
+        if (n <= 1) return n;
+        int [] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];    
     }
 }
